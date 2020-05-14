84c0472966a53123f4303184523320a185b0fbfc
Thales Lima <thaleslimadejesus@gmail.com>
2020-05-13 22:01:27 -0300

    Mudanças do escopo do projeto, e adaptações no layout

---
+++ js/scripts.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+
+    $('#linkInicio').click(function () {
+        document.getElementById('content').src = 'pages/inicio.html';
+        if (!$('#linkInicio').hasClass('active')) $('#linkInicio').toggleClass("active");
+        if ($('#linkAPI').hasClass('active')) $('#linkAPI').toggleClass("active");
+        if ($('#linkSolucoes').hasClass('active')) $('#linkSolucoes').toggleClass("active");
+    });
+
+    $('#linkAPI').click(function () {
+        document.getElementById('content').src = 'pages/api.html';
+        if (!$('#linkAPI').hasClass('active')) $('#linkAPI').toggleClass("active");
+        if ($('#linkInicio').hasClass('active')) $('#linkInicio').toggleClass("active");
+        if ($('#linkSolucoes').hasClass('active')) $('#linkSolucoes').toggleClass("active");
+    });
+
+    $('#linkSolucoes').click(function () {
+        document.getElementById('content').src = 'pages/solucoes.html';
+        if (!$('#linkSolucoes').hasClass('active')) $('#linkSolucoes').toggleClass("active");
+        if ($('#linkInicio').hasClass('active')) $('#linkInicio').toggleClass("active");
+        if ($('#linkAPI').hasClass('active')) $('#linkAPI').toggleClass("active");
+    });
+
+});
